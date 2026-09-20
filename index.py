a2689ec70df4e1065b8ab05a9a8bcee772b332b0
unknown <mohapituwane@gmail.com>
2026-09-20 16:48:20 -0700

    First Commit

---
+++ index.py
@@ -0,0 +1,5 @@
+# Ask the member for their name
+member_name =(input("Enter your name: "))
+
+# Print a message announcing the new project
+print(f"Hello, {member_name}! We are about to start a new project.")
