03f7a8ed4ba47f8b25343a8905cfa7ad2afbf038
simangem <160879411+simangem@users.noreply.github.com>
2026-09-21 21:22:17 +0200

    i added a new character to the game

--- index.py
+++ index.py
@@ -3,3 +3,8 @@ member_name =(input("Enter your name: "))
 
 # Print a message announcing the new project
 print(f"Hello, {member_name}! We are about to start a new project.")
+
+user_name = input("enter your user_name")
+school = input("enter the namme of your school")
+
+print("goodday : user_name your school is not part of our programme")
